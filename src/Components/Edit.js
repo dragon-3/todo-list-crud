62f3195b5fdb0ab085a179b62d7d2d0d617dad1f
Sean Narron <seannarron9@gmail.com>
2022-08-17 11:55:26 +0900

    Created a update function.

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import axios from 'axios';
 
 
@@ -10,6 +10,7 @@ function Edit() {
     const [event, setEvent] = useState("")
     const [time, setTime] = useState("")
     const { id } = useParams();
+    const navigate = useNavigate(); 
 
     const getData = () => {
         axios.get(`http://localhost:3003/todos/${id}`)
@@ -25,14 +26,33 @@ function Edit() {
         
     }, [])
 
+
+    const handleEventChange = (event) => {
+        event.preventDefault();
+        setEvent(event.target.value)
+    }
+
+    const handleTimeChange = (event) => {
+        event.preventDefault();
+        setTime(event.target.value)
+    }
+
+    const updateUser = () => {
+        axios.put(`http://localhost:3003/todos/${id}`, {event, time})
+        navigate("/")
+    }
+
     return(
         <div>
             <h2>Edit Item</h2>
             <form action="">
                 <label htmlFor="">Todo:</label>
-                <input type="text" value={event}/><br />
+                <input type="text" name="todo" onChange={handleEventChange} value={event}/><br />
                 <label htmlFor="">Time:</label>
-                <input type="text" value={time} />
+                <input type="text" name="time" onChange={handleTimeChange} value={time} />
+                <div className="form-submit-button">
+                    <input type="submit" onClick={updateUser}/>
+                </div>
             </form>
         </div>
     )
